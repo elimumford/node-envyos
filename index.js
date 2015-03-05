1ba4eabcb6fe6e5d40cb2f765617994a45ee4a83
Eli Mumford <elimumford@gmail.com>
2015-03-05 02:23:38 -0700

    working win32

---
+++ index.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+
+var path = require('path');
+var processor = require(path.join('./lib/', process.platform));
+
+module.exports = processor(process.env);
---
+++ test/win32-tests.js
@@ -0,0 +1,5 @@
+﻿describe("Envyos win32", function() {
+  it("works", function() {
+    var envyos = require('../lib/win32')(process.env);
+  });
+});
