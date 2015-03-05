6cf937681bb8c1074ba8bb7d61d86384f5a8c458
Eli Mumford <elimumford@gmail.com>
2015-03-05 02:52:07 -0700

    base function should handle all cases with passed regex

--- test/win32-tests.js
+++ test/win32-tests.js
@@ -1,5 +1,30 @@
 ﻿describe("Envyos win32", function() {
+  var envyos = require('../lib/win32');
+  
   it("works", function() {
-    var envyos = require('../lib/win32')(process.env);
+    var prc_env = envyos(process.env);
   });
+  
+  it('handles case miss match', function() {
+    var fakeEnv = {
+      "SOME_KEY": "TEST-VALUE",
+      "NEXT_KEY": "Next Value is not %SOME_KEY%",
+      "CASE_KEY": "Case Set Key",
+      "MISS_CASE_KEY": "some value with a case incorrect ref to %case_KEY%"
+    };
+    
+    var faked = envyos(fakeEnv);
+  });
+
+  it('handles circular refs', function() {
+    var fakeEnv = {
+      "SOME_KEY": "TEST-VALUE",
+      "NEXT_KEY": "Next Value is not %SOME_KEY%",
+      "CIRCULAR_KEY": "Key with Circular Ref to %CIRCULAR_KEY% which would suck.",
+      "MISS_CASE_KEY": "some value with a case incorrect ref to %case_KEY%"
+    };
+    
+    var faked = envyos(fakeEnv);
+  });
+
 });
