2463bd284a511a4a577076d1b5634aa0d60667d0
Eli Mumford <elimumford@gmail.com>
2015-03-12 02:16:32 -0600

    added option to force all key values on the processed object to be capitalized; this is defaulted to on for the win32 processor.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ﻿# node-envyos [![Build Status](https://travis-ci.org/elimumford/node-envyos.png?branch=master)](https://travis-ci.org/elimumford/node-envyos)
 
-Basic node.js process.env replacer that pre-expands environment variables inside other envronment variables
+Basic node.js process.env replacer that pre-expands environment variables inside other environment variables
 
 # Example
 
 ```javascript
-process.env = require('node-envyos');
+process.env = require('node-envyos')
 
+## Notes
+
+### Windows
+Because windows environment variables are not case sensative and the majority of other operating systems are and tend towards an upper case convention, the win32 processor forces all of the keys to capitalized versions in the replacement object
--- test/win32-tests.js
+++ test/win32-tests.js
@@ -14,6 +14,26 @@ describe("Envyos win32", function() {
     
     expect(original_keys.length).to.equal(processed_keys.length);
   });
+
+  it("processed object has all upper case keys", function() {
+    var test_win32_env = {
+      "some_key": "TEST-VALUE",
+      "NEXT_KEY": "Next Value is not %SOME_KEY%",
+      "CASE_key": "Case Set Key",
+      "MISS_case_KEY": "some value with a case incorrect ref to %case_KEY%"
+    };
+    
+    var processed_win32_env = underTest(test_win32_env);
+    
+    expect(processed_win32_env).to.be.ok();
+    expect(processed_win32_env).to.have.key('SOME_KEY');
+    expect(processed_win32_env).to.have.key('NEXT_KEY');
+    expect(processed_win32_env).to.have.key('CASE_KEY');
+    expect(processed_win32_env).to.have.key('MISS_CASE_KEY');
+    
+    expect(processed_win32_env.NEXT_KEY).to.contain(processed_win32_env.SOME_KEY);
+    expect(processed_win32_env.MISS_CASE_KEY).to.contain(processed_win32_env.CASE_KEY);
+  });
   
   it('is not case sensative', function() {
     var test_win32_env = {
